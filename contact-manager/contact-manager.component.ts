36255fcf434ddbff51e4c9706687eb23bc58b5c4
Riazalmahmud <riazalmahmud041@gmail.com>
2022-09-18 22:33:21 +0600

    contact

---
+++ contact-manager/contact-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-manager',
+  templateUrl: './contact-manager.component.html',
+  styleUrls: ['./contact-manager.component.scss']
+})
+export class ContactManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
